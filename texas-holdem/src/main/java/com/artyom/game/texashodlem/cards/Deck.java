3303897a7b88fa48f87ad047d7d84acbc90d8524
Zayac-The-Engineer <fed4chuck@gmail.com>
2021-09-08 11:41:05 +0300

    added javadoc

--- texas-holdem/src/main/java/com/artyom/game/texashodlem/cards/Deck.java
+++ texas-holdem/src/main/java/com/artyom/game/texashodlem/cards/Deck.java
@@ -6,18 +6,22 @@ import java.util.Stack;
 public class Deck {
     private Stack<Card> cards = new Stack<>();
 
+    /**
+     * Creates a new shuffled deck
+     */
     public void shuffle(){
         cards.clear();
-
         for(Rank rank: Rank.values()){
             for(Suit suit: Suit.values()){
                 cards.add(new Card(rank, suit));
             }
         }
-
         Collections.shuffle(cards);
     }
 
+    /**
+     * @return pops top card from deck and returns it
+     */
     public synchronized Card takeTop(){
         return cards.pop();
     }
