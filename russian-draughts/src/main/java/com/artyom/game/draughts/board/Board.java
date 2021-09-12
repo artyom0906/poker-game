83cefa61484a5bd4dea747e508df17bae2b85a8b
artyom kabakov <matroskin7@i.ua>
2021-09-12 16:13:05 +0300

    Changes by zayac

--- russian-draughts/src/main/java/com/artyom/game/draughts/board/Board.java
+++ russian-draughts/src/main/java/com/artyom/game/draughts/board/Board.java
@@ -3,8 +3,7 @@ package com.artyom.game.draughts.board;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Board extends Entity {
-
+public class Board<T> {
     public static final int BOARD_SQUARE_SCALE = 70;
     public static final int MARGIN_LEFT = 400-BOARD_SQUARE_SCALE*4;
     public static final int MARGIN_TOP = 300-BOARD_SQUARE_SCALE*4;
